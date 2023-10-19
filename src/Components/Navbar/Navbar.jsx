86a261e59b42e87f3ef85b924661e9fcbe3ffa06
habib162 <mrahman162123@bscse.uiu.ac.bd>
2023-10-19 22:16:31 +0600

    footer added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
         document.querySelector("html").setAttribute("data-theme", localTheme);
     }, [theme]);
     return (
-        <div className="navbar bg-base-100  shadow-lg px-4 sm:px-8">
+        <div className="navbar bg-base-100 py-4 shadow-lg px-4 sm:px-8">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -4,7 +4,7 @@ function Slider() {
         <div>
             <div className="carousel w-full h-[630px]">
                 <div id="slide1" className="carousel-item relative w-full ">
-                    <div className="hero h-[630px]" style={{ backgroundImage: 'url(https://i.ibb.co/Q8R16YK/cover1.jpg)' }}>
+                    <div className="hero h-[630px]" style={{ backgroundImage: 'url(https://i.ibb.co/w00pPXd/bg-3.jpg)' }}>
                         <div className="hero-overlay bg-opacity-60"></div>
                         <div className="hero-content text-center text-neutral-content">
                             <div className="max-w-md">
@@ -19,7 +19,7 @@ function Slider() {
                     </div>
                 </div>
                 <div id="slide2" className="carousel-item relative w-full">
-                    <div className="hero h-[630px]" style={{ backgroundImage: 'url(https://i.ibb.co/RyXvyXm/cover2.jpg)' }}>
+                    <div className="hero h-[630px]" style={{ backgroundImage: 'url(https://i.ibb.co/FxNyMGK/bg-2.jpg)' }}>
                         <div className="hero-overlay bg-opacity-60"></div>
                         <div className="hero-content text-center text-neutral-content">
                             <div className="max-w-md">
@@ -34,7 +34,7 @@ function Slider() {
                     </div>
                 </div>
                 <div id="slide3" className="carousel-item relative w-full">
-                    <div className="hero h-[630px]" style={{ backgroundImage: 'url(https://i.ibb.co/jWzfvS7/cover3.jpg)' }}>
+                    <div className="hero h-[630px]" style={{ backgroundImage: 'url(https://i.ibb.co/k1PZZpS/bg-1.jpg)' }}>
                         <div className="hero-overlay bg-opacity-60"></div>
                         <div className="hero-content text-center text-neutral-content">
                             <div className="max-w-md">
